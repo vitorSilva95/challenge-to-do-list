4e0c82960c7dc750119a309cba8fc451e476e25c
Vitor Silva <vitor.o.silva95@gmail.com>
2022-06-07 19:15:08 -0300

    feat: Implementa menssagens de erro personalizadas

--- app/backend/src/interfaces/TodoList.ts
+++ app/backend/src/interfaces/TodoList.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
 export const TodoListSchema = z.object({
-  task: z.string().nonempty({ message: 'Task cannot be empty'}),
+  task: z.string({
+    required_error: 'Task is required',
+    invalid_type_error: 'Task must be a string',
+  }).nonempty({ message: 'Task cannot be empty'}),
   isDone: z.boolean().default(false),
 });
 
-export type TodoList = z.infer<typeof TodoListSchema>
+export type TodoList = z.infer<typeof TodoListSchema>;
+
+export type TodoListUpdate = Partial<TodoList>;
+
