fcdd01105e3d2360529b3d3e76dbe033b54495dc
Vitor Silva <vitor.o.silva95@gmail.com>
2022-06-07 19:18:08 -0300

    feat: Implementa metodos update e delete no controller

--- app/backend/src/app/controllers/todoListController.ts
+++ app/backend/src/app/controllers/todoListController.ts
@@ -14,7 +14,17 @@ class TodoListController {
   async create(body:TodoList) {
     const result = await this.service.create(body);
     return result;
+  }
+
+  async update(id: string, body:TodoList) {
+    const result = await this.service.update(id, body);
+    return result;
+  }
+  async delete(id: string) {
+    const result = await this.service.delete(id);
+    return result;
   } 
+
 }
 
 export default TodoListController;
