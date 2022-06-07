4f82d41f98b06598b4e64c45f898d5fa727fe72a
Vitor Silva <vitor.o.silva95@gmail.com>
2022-06-07 19:18:33 -0300

    feat: Implementa rotas put e delete

--- app/backend/src/api/routes/todoListRouter.ts
+++ app/backend/src/api/routes/todoListRouter.ts
@@ -9,9 +9,19 @@ todoListRouter.get('/',async (_req,res) =>{
   const result = await todoListController.getAll();
   return res.status(200).json(result);
 } );
-todoListRouter.post('/',validateBody, async (req,res)=> {
+todoListRouter.post('/', validateBody, async (req,res)=> {
   const result = await todoListController.create(req.body);
   return res.status(201).json(result);
+});
+todoListRouter.put('/:id', async (req,res)=> {
+  const { id } = req.params;
+  const result = await todoListController.update(id, req.body);
+  return res.status(200).json(result);
+});
+todoListRouter.delete('/:id', async (req,res)=> {
+  const { id } = req.params;
+  const result = await todoListController.delete(id);
+  return res.status(200).json(result);
 }); 
 
 export default todoListRouter;
